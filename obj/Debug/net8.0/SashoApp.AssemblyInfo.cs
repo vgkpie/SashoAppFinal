ce77fe6597de243300a63ade83e7e46121c98b95
vgkpie <davidofv1056@gmail.com>
2025-05-22 00:15:53 +0300

    Final

--- obj/Debug/net8.0/SashoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SashoApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SashoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726282e7580e33b53934011fe983ddb969ae5e31")]
 [assembly: System.Reflection.AssemblyProductAttribute("SashoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SashoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
